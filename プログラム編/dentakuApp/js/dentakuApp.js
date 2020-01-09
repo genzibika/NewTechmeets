734fb86e8d8781025af456401a75b479979c628a
genzibika <genzibika.0906@gmail.com>
2020-01-09 23:29:04 +0900

    jsでボタンのidを取得

---
+++ プログラム編/dentakuApp/js/dentakuApp.js
@@ -0,0 +1,22 @@
+// 式と合計のdocument.getElementById()
+var total = document.getElementById('total');
+// 0~9のdocument.getElementById()
+var number = document.getElementById('number');
+// .(小数点)のdocument.getElementById()
+var few = document.getElementById('few');
+// Cのdocument.getElementById()
+var clear = document.getElementById('clear');
+// ±のdocument.getElementById()
+var chaeng = document.getElementById('chaeng');
+// %のdocument.getElementById()
+var percent = document.getElementById('percent');
+// ÷のdocument.getElementById()
+var division = document.getElementById('division');
+// ×のdocument.getElementById()
+var multiplication = document.getElementById('multiplication');
+// -のdocument.getElementById()
+var subtraction = document.getElementById('subtraction');
+// +のdocument.getElementById()
+var addition = document.getElementById('addition');
+// =のdocument.getElementById()
+var equal = document.getElementById('equal');
