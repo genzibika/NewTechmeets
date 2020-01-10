66b7475799a904557baa2c1ed393ca2977997cc8
genzibika <genzibika.0906@gmail.com>
2020-01-10 22:28:35 +0900

    0~9のボタンをクリックした時にdivの子要素<p>としての出力処理

--- プログラム編/dentakuApp/js/dentakuApp.js
+++ プログラム編/dentakuApp/js/dentakuApp.js
@@ -1,7 +1,16 @@
 // 式と合計のdocument.getElementById()
 var total = document.getElementById('total');
 // 0~9のdocument.getElementById()
-var number = document.getElementById('number');
+var number0 = document.getElementById('number0');
+var number1 = document.getElementById('number1');
+var number2 = document.getElementById('number2');
+var number3 = document.getElementById('number3');
+var number4 = document.getElementById('number4');
+var number5 = document.getElementById('number5');
+var number6 = document.getElementById('number6');
+var number7 = document.getElementById('number7');
+var number8 = document.getElementById('number8');
+var number9 = document.getElementById('number9');
 // .(小数点)のdocument.getElementById()
 var few = document.getElementById('few');
 // Cのdocument.getElementById()
@@ -20,3 +29,57 @@ var subtraction = document.getElementById('subtraction');
 var addition = document.getElementById('addition');
 // =のdocument.getElementById()
 var equal = document.getElementById('equal');
+
+// ============================================
+// 0~9をdivの子要素<p>として出力する処理
+// 冗長なので他の処理を後に探す。
+number0.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number0.value;
+  total.appendChild(P);
+}
+number1.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number1.value;
+  total.appendChild(P);
+}
+number2.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number2.value;
+  total.appendChild(P);
+}
+number3.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number3.value;
+  total.appendChild(P);
+}
+number4.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number4.value;
+  total.appendChild(P);
+}
+number5.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number5.value;
+  total.appendChild(P);
+}
+number6.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number6.value;
+  total.appendChild(P);
+}
+number7.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number7.value;
+  total.appendChild(P);
+}
+number8.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number8.value;
+  total.appendChild(P);
+}
+number9.onclick = function(){
+  var P = document.createElement('p');
+  P.innerHTML = number9.value;
+  total.appendChild(P);
+}
